ea3eebc8712caf80bd083d0b9b5b5a6e7c18f4dc
中村圭輝 <mac@nakamuranomac.local>
2020-03-24 23:16:05 +0900

    新規プロジェクト tasklist

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {return redirect('/tasks');});
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
+
+// paizaCloudではhttpsにする必要があったが、ローカル環境では不要
+// \URL::forceScheme('https');
